b874a37b68de0abeff3285ee5257308f48832d37
Jay <j99590314@gmail.com>
2023-02-07 20:39:22 +0800

    feat: revised set

--- set/set.go
+++ set/set.go
@@ -4,25 +4,25 @@ type Set struct {
 	container map[int]struct{}
 }
 
-func NewSet() Set {
-	return Set{
+func NewSet() *Set {
+	return &Set{
 		container: make(map[int]struct{}),
 	}
 }
 
-func (set Set) Add(x int) {
+func (set *Set) Add(x int) {
 	set.container[x] = struct{}{}
 }
 
-func (set Set) Remove(x int) {
+func (set *Set) Remove(x int) {
 	delete(set.container, x)
 }
 
-func (set Set) Len() int {
+func (set *Set) Len() int {
 	return len(set.container)
 }
 
-func (set Set) Contains(x int) bool {
+func (set *Set) Contains(x int) bool {
 	_, found := set.container[x]
 	return found
 }
